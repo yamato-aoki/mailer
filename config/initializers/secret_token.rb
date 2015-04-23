d56b85da63bc2c86933b25a9385c09107c4fd928
yamato.aoki <yamato.aoki@livesense.co.jp>
2015-04-23 15:51:10 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mailer::Application.config.secret_key_base = 'a6d38ab001dae4fd831d2a4378b539e3a4ea35dd9221efc0b4c5642d2fa3303657ca1e27c3322b1025dc95fd9dbe90b86fc49a4ed0d69090f73f26c259cf1e23'
